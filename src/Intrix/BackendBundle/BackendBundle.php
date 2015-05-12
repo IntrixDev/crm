0a44d4a0b1036b69ae4a4da63239e3194b50ef6d
jean <jean@jean-PC>
2015-05-12 11:16:35 -0300

    Adicionando criando base

---
+++ src/Intrix/BackendBundle/BackendBundle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Intrix\BackendBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BackendBundle extends Bundle {
+
+    public function getParent() {
+        return 'FOSUserBundle';
+    }
+
+}
